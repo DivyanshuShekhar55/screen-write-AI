31b70e0b7d67908a656577a7b1d7dd83373427c7
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-10-11 19:35:00 +0530

    added the transparent screen feature

--- screen-write-AI/src/App.jsx
+++ screen-write-AI/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 function App() {
   return (
-    <div className='p-8 bg-green-300'>
+    <div className='p-8'>
       <h1 className='text-red-600' >Hello World</h1>
     </div>
   )
