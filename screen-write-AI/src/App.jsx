1044a35aec665520e9afa50f4e4e2ed34ba7592e
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-10-11 19:26:31 +0530

    Initial setup done

---
+++ screen-write-AI/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='p-8 bg-green-300'>
+      <h1 className='text-red-600' >Hello World</h1>
+    </div>
+  )
+}
+
+export default App
