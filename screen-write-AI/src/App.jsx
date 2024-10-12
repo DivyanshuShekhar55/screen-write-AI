f9b8584e0206d87e2dab94bfdbb34a7203bd8882
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-10-12 13:24:07 +0530

    added the canvas with some neon draw brush.

--- screen-write-AI/src/App.jsx
+++ screen-write-AI/src/App.jsx
@@ -1,11 +1,66 @@
 import React from 'react'
 import './App.css'
 import Sidebar from './components/Sidebar'
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.globalAlpha = 1;
+    ctx.strokeStyle = "magenta";
+    ctx.lineWidth = 0.5;
+    ctxRef.current = ctx;
+
+    ctx.shadowBlur = 7;  // Adds glow effect
+    ctx.shadowColor = "rgba(255, 0, 255, 0.4)";  // Glow color
+  }, []);
+
+  const startDrawing = (e) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY
+    );
+    setIsDrawing(true);
+  }; const endDrawing = () => {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  const draw = (e) => {
+    if (!isDrawing) {
+      return;
+    }
+    ctxRef.current.lineTo(
+      e.nativeEvent.offsetX,
+      e.nativeEvent.offsetY
+    );
+
+    ctxRef.current.stroke();
+  };
+
   return (
     <div className='p-8 static'>
-      <Sidebar/>
+      <Sidebar />
+
+      <div className=''>
+
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={endDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+        width="1600"
+        height="600"
+      />
+      </div>
     </div>
   )
 }
