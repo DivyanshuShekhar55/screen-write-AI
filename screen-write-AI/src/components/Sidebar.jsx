3bc47f3f715b4c5e851aaa2d6d9eca5921d6a9a0
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-10-13 17:25:56 +0530

    added more sidebar buttons and related assests

--- screen-write-AI/src/components/Sidebar.jsx
+++ screen-write-AI/src/components/Sidebar.jsx
@@ -5,6 +5,9 @@ import handImg from '../assets/image.png'
 import redCircle from '../assets/redCircle.png'
 import blueCircle from '../assets/blueCircle.png'
 import cut from '../assets/cut.png'
+import erase from '../assets/eraser.png'
+import vanishing from "../assets/vanishing.png"
+
 
 const icons = [
     {
@@ -14,11 +17,6 @@ const icons = [
         text: "Ask ChatGPT"
     },
     {
-        id: 5,
-        name: "Hold",
-        icon: handImg,
-        text: "Hold Images"
-    }, {
         id: 2,
         name: "Snip",
         icon: cut,
@@ -26,25 +24,44 @@ const icons = [
     },
     {
         id: 3,
+        name: "Vanish",
+        icon: vanishing,
+        text: "Vansih Mode"
+    },
+    {
+        id: 4,
+        name: "Hold",
+        icon: handImg,
+        text: "Hold Images"
+    },
+    {
+        id: 5,
+        name: "Erase",
+        icon: erase,
+        text: "Erase Sketch"
+    },
+    {
+        id: 6,
         name: "Red",
         icon: redCircle,
         text: "Red Pen"
     },
     {
-        id: 4,
+        id: 7,
         name: "Blue",
         icon: blueCircle,
         text: "Blue Pen"
-    }
+    },
+    
 
 ]
 
 function Sidebar() {
     return (
-        <div className='absolute top-1/3 right-8 justify-center '>
+        <div className='absolute top-1/4 right-8 justify-center '>
             <div className='flex flex-col gap-5'>
-                {icons.map(({icon, text}) => (
-                    <SidebarButton icon={icon} text={text}/>
+                {icons.map(({ icon, text }) => (
+                    <SidebarButton icon={icon} text={text} />
                 ))}
 
             </div>
