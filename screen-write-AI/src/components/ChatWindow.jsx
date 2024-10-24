0af5af0cd6a95c8d9e6f7385b32de159271b0780
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-10-24 20:21:32 +0530

    added the text bar UI for ai chat window

--- screen-write-AI/src/components/ChatWindow.jsx
+++ screen-write-AI/src/components/ChatWindow.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import AIwindowHeader from './AIwindowHeader'
 import SwitchButton from './SwitchButton'
+import AItextBar from './AItextBar'
 
 function ChatWindow() {
     return (
-        <div className='bg-gray-900 text-white w-3/5 h-2/3 z-10 absolute top-1/4 left-1/4 rounded-2xl'>
+        <div className='bg-gray-900 text-white w-3/5 h-2/3 z-10 absolute top-1/4 left-1/4 rounded-3xl flex flex-col justify-between'>
             <div className='w-full h-[10%]'>
                 <AIwindowHeader />
             </div>
+
+            <div className='w-[90%] h-[10%] my-[5%] m-auto'>
+                <AItextBar/>
+            </div>
         </div>
     )
-
 }
 
 export default ChatWindow
