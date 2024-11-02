e290c2e32454d981d0642c1f7a8edefc93878d1b
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-11-02 18:11:14 +0530

    added better UI to the Screenshot component

--- screen-write-AI/src/components/ScreenCapture.jsx
+++ screen-write-AI/src/components/ScreenCapture.jsx
@@ -8,7 +8,7 @@ function ScreenCapture() {
     const handleMouseDown = (e) => {
         const startX = e.clientX
         const startY = e.clientY
-        setSelectStartPos({x:e.clientX, y:e.clientY})
+        setSelectStartPos({ x: e.clientX, y: e.clientY })
         setisDragging(true)
     }
 
@@ -20,34 +20,39 @@ function ScreenCapture() {
 
     const handleMouseUp = () => {
         setisDragging(false)
-        alert(`started at ${selectStartPos.x} ended at ${currPos.x}`)
+
+        //reset the states so that in the next drag event we don't see the last pos as start of the rect
+        setSelectStartPos({ x: null, y: null })
+        setCurrPos({ x: null, y: null })
     }
 
 
     return (
 
-        <div 
-            className='w-3/4 h-3/4 bg-pink-400 cursor-crosshair' 
+        <div
+            className='w-screen h-screen cursor-pointer'
             onMouseDown={handleMouseDown}
             onMouseMove={handleDrag}
             onMouseUp={handleMouseUp}
-            // Add more event handlers as needed
+        // Add more event handlers as needed
         >
             {isDragging && selectStartPos.x !== null && currPos.x !== null && (
-                <div 
+                <div
                     style={{
                         position: 'fixed',
                         left: Math.min(selectStartPos.x, currPos.x),
                         top: Math.min(selectStartPos.y, currPos.y),
                         width: Math.abs(currPos.x - selectStartPos.x),
                         height: Math.abs(currPos.y - selectStartPos.y),
-                        border: '2px solid #00ff00',
-                        backgroundColor: 'rgba(0, 255, 0, 0.1)',
+                        border: '2px solid  rgba(188, 170, 240, 0.9)',
+                        backgroundColor: 'rgba(227, 215, 255, 0.25)',
+                        boxShadow:'0 0 10px rgba(188, 170, 240, 0.8), 0 0 20px rgba(188, 170, 240, 0.6), 0 0 30px rgba(188, 170, 240, 0.4)',
+                        borderRadius:'10px',
                         pointerEvents: 'none'  // Prevents the rectangle from interfering with events
                     }}
                 />
             )}
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi qui nemo temporibus earum consequuntur nobis ipsa dolorum non provident eaque sunt, omnis enim dolorem, aspernatur maiores laboriosam. Assumenda ducimus odit eum ipsam, a distinctio laborum animi soluta numquam. Velit dolorum itaque eum dicta deserunt quos harum laborum veritatis unde aliquid.
+
         </div>
     )
 }
