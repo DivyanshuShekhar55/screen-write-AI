fbcd22952a08894448ee74141c9873bdb890639e
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-11-03 08:50:11 +0530

    actually fixed the invoke method import now :-)

--- screen-write-AI/src/components/ScreenCapture.jsx
+++ screen-write-AI/src/components/ScreenCapture.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { invoke } from '@tauri-apps/api/core';
 
 function ScreenCapture() {
     const [selectStartPos, setSelectStartPos] = useState({ x: null, y: null })
