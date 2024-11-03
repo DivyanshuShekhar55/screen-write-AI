41cae5715411b01fef7e5167ebf4a34129bcec36
DivyanshuShekhar55 <divyanshushekhar987@gmail.com>
2024-11-03 10:48:25 +0530

    connected the backend and frontends for screenshot vcapture

--- screen-write-AI/src-tauri/src/lib.rs
+++ screen-write-AI/src-tauri/src/lib.rs
@@ -1,14 +1,45 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use screenshots::Screen;
+use std::io::Cursor;
+use base64::engine::general_purpose::STANDARD as BASE64_STANDARD;
+use base64::Engine;
+use screenshots::image::ImageOutputFormat;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+async fn capture_screenshot(x:i32, y:i32, width:u32, height:u32)->Result<String, String> {
+    let screens = Screen::all().map_err(|e| e.to_string())?;
+
+    // just taking the primary screen's shot for now ...
+    // may be extended for multiple screes / monitors' screen
+    let screen = screens.first().ok_or("No screen found")?;
+
+    let image = screen
+        .capture_area(x, y, width, height)
+        .map_err(|e| e.to_string())?;
+
+    // Convert the image to PNG bytes
+    let mut png_data: Vec<u8> = Vec::new();
+    image.write_to(&mut Cursor::new(&mut png_data), ImageOutputFormat::Png)
+        .map_err(|e| e.to_string())?;
+    
+    // Convert to base64
+    let base64_string = BASE64_STANDARD.encode(png_data);
+    
+    // Return the base64 string with data URL prefix
+    Ok(format!("data:image/png;base64,{}", base64_string))
+
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, capture_screenshot])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- screen-write-AI/src/components/ScreenCapture.jsx
+++ screen-write-AI/src/components/ScreenCapture.jsx
@@ -2,9 +2,33 @@ import React, { useState } from 'react'
 import { invoke } from '@tauri-apps/api/core';
 
 function ScreenCapture() {
+
+    const max_ss_limit = import.meta.env.MAX_SCREENSHOT_LIMIT
+
     const [selectStartPos, setSelectStartPos] = useState({ x: null, y: null })
     const [currPos, setCurrPos] = useState({ x: null, y: null })
     const [isDragging, setisDragging] = useState(false)
+    const [screenshots, setScreenshots] = useState([]);
+    const [isCapturing, setIsCapturing] = useState(false);
+
+    const handleScreenCapture = async (region) => {
+        try {
+            setIsCapturing(true)
+            const base64Img = await invoke("capture_screenshot", region)
+
+            setScreenshots(prevScreenshots => {
+                const newScreenshot = [...prevScreenshots, base64Img]
+                return newScreenshot.slice(-max_ss_limit)
+
+            })
+            alert(screenshots)
+        } catch (error) {
+            alert(error)
+        } finally {
+            setIsCapturing(false);
+        }
+
+    }
 
     const handleMouseDown = (e) => {
         const startX = e.clientX
@@ -19,12 +43,24 @@ function ScreenCapture() {
         }
     }
 
-    const handleMouseUp = () => {
+    const handleMouseUp = async () => {
         setisDragging(false)
+
+        // CALL THE SCREEN CAP FUNC.
+        let h = Math.abs(currPos.y - selectStartPos.y)
+        let w = Math.abs(currPos.x - selectStartPos.x)
+        let region = {
+            x: Math.min(selectStartPos.x, currPos.x),
+            y: Math.min(selectStartPos.y, currPos.y),
+            width: w,
+            height: h
+        }
+        handleScreenCapture(region)
 
         //reset the states so that in the next drag event we don't see the last pos as start of the rect
         setSelectStartPos({ x: null, y: null })
         setCurrPos({ x: null, y: null })
+
     }
 
 
@@ -47,8 +83,8 @@ function ScreenCapture() {
                         height: Math.abs(currPos.y - selectStartPos.y),
                         border: '2px solid  rgba(188, 170, 240, 0.9)',
                         backgroundColor: 'rgba(227, 215, 255, 0.25)',
-                        boxShadow:'0 0 10px rgba(188, 170, 240, 0.8), 0 0 20px rgba(188, 170, 240, 0.6), 0 0 30px rgba(188, 170, 240, 0.4)',
-                        borderRadius:'10px',
+                        boxShadow: '0 0 10px rgba(188, 170, 240, 0.8), 0 0 20px rgba(188, 170, 240, 0.6), 0 0 30px rgba(188, 170, 240, 0.4)',
+                        borderRadius: '10px',
                         pointerEvents: 'none'  // Prevents the rectangle from interfering with events
                     }}
                 />
